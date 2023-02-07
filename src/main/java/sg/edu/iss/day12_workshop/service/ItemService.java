2e40b34143bccbdb213ccb18d25bcb6f9ab30732
Charleswcl <wang90cl@hotmail.com>
2023-02-07 13:44:08 +0800

    Time, Items & Weather Controller

---
+++ src/main/java/sg/edu/iss/day12_workshop/service/ItemService.java
@@ -0,0 +1,21 @@
+package sg.edu.iss.day12_workshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sg.edu.iss.day12_workshop.model.Item;
+import sg.edu.iss.day12_workshop.repository.ItemRepo;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    ItemRepo itmRepo;
+
+    public List<Item> retrieveItemList() {
+        return itmRepo.getCartItems();
+    }
+    
+}
